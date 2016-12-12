6af3b76fd7ea351a3a0e9c18cfa5a8909c515a35
lumurillo <luismurirojas@gmail.com>
2016-12-11 18:55:23 -0600

    Add the MODIS data to a new layer over the map

--- Leaflet/maptest.js
+++ Leaflet/maptest.js
@@ -2,12 +2,15 @@
  * Created by luisalonsomurilloalonso on 11/12/16.
  */
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([9.934739, -84.087502], 8);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 16,
     id: 'mapbox.satellite',
     accessToken: 'pk.eyJ1IjoibHVtdXJpbGxvIiwiYSI6IlVRTlZkbFkifQ.nFkWwVMJm_5mUy-9ye65Og'
 }).addTo(mymap);
+
+var geojsonLayer = new L.GeoJSON.AJAX("https://forestguardian.file.core.windows.net/modis/central_america.json");
+geojsonLayer.addTo(mymap);
 
