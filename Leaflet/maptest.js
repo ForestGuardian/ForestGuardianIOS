287a4b7ee3a29967b307d17f6ab1adb40728c0e3
lumurillo <luismurirojas@gmail.com>
2017-01-29 14:29:08 -0600

    Add a script to update the MODIS data and optimize the map for mobile

--- Leaflet/maptest.js
+++ Leaflet/maptest.js
@@ -2,7 +2,7 @@
  * Created by luisalonsomurilloalonso on 11/12/16.
  */
 
-var mymap = L.map('mapid').setView([9.934739, -84.087502], 8);
+var mymap = L.map('map').setView([9.934739, -84.087502], 8);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -11,6 +11,8 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoibHVtdXJpbGxvIiwiYSI6IlVRTlZkbFkifQ.nFkWwVMJm_5mUy-9ye65Og'
 }).addTo(mymap);
 
-var geojsonLayer = new L.GeoJSON.AJAX("https://forestguardian.file.core.windows.net/modis/central_america.json");
+mymap.locate({setView: true, maxZoom: 10});
+
+var geojsonLayer = new L.GeoJSON.AJAX("http://forestdev6339.cloudapp.net/Leaflet/central_america.json");
 geojsonLayer.addTo(mymap);
 
