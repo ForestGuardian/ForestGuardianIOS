112afa9b18c27cbc89364af819c406890e213eb0
Luis Alonso Murillo Rojas <luismurirojas@gmail.com>
2017-03-05 15:13:10 -0600

    Rename the project and remove the map box integration

---
+++ Android/GuardianForestal/app/src/main/java/org/forestguardian/LoginActivity.java
@@ -0,0 +1,54 @@
+package org.forestguardian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.UpdateManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        checkForUpdates();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkForCrashes();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterManagers();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterManagers();
+    }
+
+    private void checkForCrashes() {
+        CrashManager.register(this);
+    }
+
+    private void checkForUpdates() {
+        // Remove this for store builds!
+        UpdateManager.register(this);
+    }
+
+    private void unregisterManagers() {
+        UpdateManager.unregister();
+    }
+}
