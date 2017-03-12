3e2cccbf251868729a4a0b1a1ddd316e1bd9f60a
Luis Alonso Murillo Rojas <luismurirojas@gmail.com>
2017-03-12 13:53:04 -0600

    Add a popup message for the wildfire points

--- Leaflet/maptest.js
+++ Leaflet/maptest.js
@@ -13,16 +13,63 @@ var currentFireCoordinates = null;
  var fireIcon = L.icon({
      iconUrl: 'fire.png',
      iconSize:     [32, 37],
-     iconAnchor:   [16, 36]
+     iconAnchor:   [16, 36],
+     popupAnchor:  [0, -37]
  });
 
  function onEachFeature(feature, layer) {
      layer.setIcon(fireIcon);
+     if (feature.properties) {
+       var link = '<br><a href="javascript:displayWildfiresDetails('
+                  + feature.properties.LATITUDE + ','
+                  + feature.properties.LONGITUDE + ','
+                  + feature.properties.BRIGHTNESS + ','
+                  + feature.properties.SCAN + ','
+                  + feature.properties.TRACK + ','
+                  + '\'' + feature.properties.ACQ_DATE + '\','
+                  + '\'' + feature.properties.ACQ_TIME + '\','
+                  + '\'' + feature.properties.SATELLITE + '\','
+                  + feature.properties.CONFIDENCE + ','
+                  + '\'' + feature.properties.VERSION + '\','
+                  + feature.properties.BRIGHT_T31 + ','
+                  + feature.properties.FRP + ','
+                  + '\'' + feature.properties.DAYNIGHT + '\''
+                  + ');">Details</a>';
+
+       layer.bindPopup("<b>Wildfire point</b>"
+                      + "<br>BRIGHTNESS: " + feature.properties.BRIGHTNESS
+                      + "<br>DATE: " + feature.properties.ACQ_DATE
+                      + "<br>TIME: " + feature.properties.ACQ_TIME
+                      + "<br>SATELLITE: " + feature.properties.SATELLITE
+                      + link);
+     }
      layer.on('click', function (e) {
        map.setView(e.latlng, 13);
        currentFireCoordinates = L.latLng(e.latlng.lat, e.latlng.lng);
-       mobile.getMODISData(JSON.stringify(feature.properties));
     });
+ }
+
+ function displayWildfiresDetails(lat, lng, brightness, scan, track, date, time, satellite, confidence, version, bright_t31, frp, daynight) {
+   var jsonMODIS = {"LATITUDE":lat,
+                    "LONGITUDE":lng,
+                    "BRIGHTNESS":brightness,
+                    "SCAN":scan,
+                    "TRACK":track,
+                    "ACQ_DATE":date,
+                    "ACQ_TIME":time,
+                    "SATELLITE":satellite,
+                    "CONFIDENCE":confidence,
+                    "VERSION":version,
+                    "BRIGHT_T31":bright_t31,
+                    "FRP":frp,
+                    "DAYNIGHT":daynight};
+   console.log(JSON.stringify(jsonMODIS));
+   try {
+     mobile.getMODISData(JSON.stringify(jsonMODIS));
+   }
+   catch(err) {
+     console.log("Error trying to invoke mobile method");
+   }
  }
 
  function setUserCurrentLocation(latitude, longitude) {
